af6c9d69f294ef6dde8864927b332ccef97f3271
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2020-10-22 00:25:03 +0100

    Exploring the datasets (#1)
    * Correlation matrix of census datasets
    * Functions for dataset loading, cleaning and normalising
    
    Co-authored-by: Antros Economides <antroseconomides@hotmail.com>

--- src/visulisation.py
+++ src/visulisation.py
@@ -1,7 +1,12 @@
-import numpy as np
-import seaborn as sns
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+import dataset
 
 
+census: pd.DataFrame = dataset.load_census_data()
 
+sns.heatmap(census.drop(columns=['geoid', 'state', 'county', 'tract']).corr())
+
+plt.show()
