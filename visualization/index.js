3e0f28ac4d9df74ac239492bfb1bf9d24c79e9e2
W Zhang <wz315@cam.ac.uk>
2020-10-25 15:58:29 +0000

    Displaying data from different years

--- visualization/index.js
+++ visualization/index.js
@@ -1,7 +1,20 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoid2VpeHVhbnoiLCJhIjoiY2tna3lkMnhjMWNubzMwdGVrN3ZtaHpqOSJ9.2ntrcdRtEDlAuW1OkflN4g';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2VpeHVhbnoiLCJhIjoiY2tna3lkMnhjMWNubzMwdGVrN3ZtaHpqOSJ9.2ntrcdRtEDlAuW1OkflN4g";
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-73.937724, 40.711725], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  container: "map",
+  style: "mapbox://styles/weixuanz/ckgo3bk222iew19quuk25vjwf",
+  center: [-73.937724, 40.711725],
+  zoom: 10,
 });
+
+var layerList = document.getElementById("menu");
+var inputs = layerList.getElementsByTagName("input");
+
+function switchLayer(layer) {
+  var layerId = layer.target.value;
+  map.setStyle("mapbox://styles/weixuanz/" + layerId);
+}
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].onclick = switchLayer;
+}
